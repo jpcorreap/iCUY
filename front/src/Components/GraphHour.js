4e2d75ff4b93871f5574bb53dd91a345d476c153
Matt <jm.contreras10@uniandes.edu.co>
2020-05-11 10:51:15 -0500

    NumberGraph

---
+++ front/src/Components/GraphHour.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Chart from 'react-apexcharts'
+
+const GraphHour = (props) => {
+
+    const data = props.data
+
+    console.log(data)
+
+    data.sort((a, b) => {
+        let AEl = new Date(a.date), BEl = new Date(b.date);
+        if (AEl < BEl) return -1;
+        if (AEl > BEl) return 1;
+        return 0;
+    });
+
+    const options = {
+        chart: {
+            id: 'Dates'
+        },
+        xaxis: {
+            categories: data.map(record => record.date)
+        }
+    }
+
+    const series = [{
+        name: 'Hour',
+        data: data.map(record => record.value)
+    }]
+
+    return (
+        <div className = "w-100 h-75 position-absolute bottom-0 ">
+            <Chart options={options} series={series} type="line" width={"80%"} height={"100%"} />
+        </div>
+    )
+
+}
+
+export default GraphHour
+
